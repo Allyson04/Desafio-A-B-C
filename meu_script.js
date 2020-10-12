f0928dd40ed29d8c1830a5450cc3d29eea06f693
Allyson <allysoneduardo04@gmail.com>
2020-10-11 21:32:27 -0300

    Separado Page do codigo javascript

--- meu_script.js
+++ meu_script.js
@@ -1,2 +1,12 @@
-alert('Uopa')
-document.getElementById('nome').value = 'Oi'
+var a = 10;
+var b = 20;
+var c = null;
+
+c = a;
+a = b;
+a = c;
+c = "<br />";
+
+document.write(a + c);
+document.write(b + c);
+         
